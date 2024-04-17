e6aa9f6fd596bb407a0e1ae559e5ab021b572b84
Никита Бойко <madisan2099@gmail.com>
2024-04-17 22:32:48 +0300

    Add project files.

---
+++ BookShopApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookShopApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
